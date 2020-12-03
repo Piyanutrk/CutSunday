b1e1fc0c139f5613291a1510bc80b5a510094ff7
piyanut.rk <piyanut.rk@gmail.com>
2020-12-03 22:06:23 +0700

    add comment

--- CutSunday/Program.cs
+++ CutSunday/Program.cs
@@ -10,36 +10,44 @@ namespace CutSunday
     {
         static void Main(string[] args)
         {
-            DateTime today = DateTime.Today;
+            DateTime today = DateTime.Today;  // ประกาศตัวแปรชนิด DateTime กำหนดค่า เป็นวันปัจจุบัน
 
-            string[] day = new string[20];
+            string[] day = new string[20]; // ประกาศตัวแปรแบบอาเรย์ชนิด string ความยาว 20
 
-            for (int i = 0; i < day.Length; i++)
+            for (int i = 0; i < day.Length; i++)  // for loop จำนวนครั้งตามความยาวของตัวแปร day
             {
-                if (i == 0)
+                if (i == 0) // เช็คว่าเป็นตำแหน่งแรกของอาเรย์
                 {
-                    day[i] = today.DayOfWeek.ToString();
+                    // yes
+                    
+                    day[i] = today.DayOfWeek.ToString(); // เก็บค่าวันเช่น Monday ลงในอาเรย์
                 }
-                else
+                else // ถ้าไม่ใช่ตำแหน่งแรกของอาเรย์
                 {
-                    today = today.AddDays(1);
+                    // no
 
-                    if (today.DayOfWeek == DayOfWeek.Sunday)
+                    today = today.AddDays(1);   // เพิ่มวัน 1 วัน ให้กับตัวแปร today
+
+                    if (today.DayOfWeek == DayOfWeek.Sunday)    // หลังจากเพิ่มวันก็เช็คว่าเป็นวันอาทิตย์ไหม
                     {
-                        today = today.AddDays(1);
+                        // yes
 
-                        day[i] = today.DayOfWeek.ToString();
+                        today = today.AddDays(1);   // เพิ่มวันอีก 1 วันให้กับตัวแปร today
+
+                        day[i] = today.DayOfWeek.ToString();    // เก็บค่าวันเช่น Monday ลงในอาเรย์
                     }
                     else
                     {
-                        day[i] = today.DayOfWeek.ToString();
+                        // no
+
+                        day[i] = today.DayOfWeek.ToString();    // เก็บค่าวันเช่น Monday ลงในอาเรย์
                     }
                 }                
             }
 
-            foreach (var item in day)
+            foreach (var item in day)   // loop foreach อ่านค่าในตัวแปรเดย์ มาเก็[ที่ตัวแปร item วนตามความยาวของตัวแปร day
             {
-                Console.WriteLine(item);
+                Console.WriteLine(item);    // คำสั่ง print
             }
 
 

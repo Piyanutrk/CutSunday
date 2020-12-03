c29f0bd3dbe67561d3444f08b942d03fa858ba97
piyanut.rk <piyanut.rk@gmail.com>
2020-12-03 21:57:04 +0700

    Add project files.

---
+++ CutSunday/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutSunday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;
+
+            string[] day = new string[20];
+
+            for (int i = 0; i < day.Length; i++)
+            {
+                if (i == 0)
+                {
+                    day[i] = today.DayOfWeek.ToString();
+                }
+                else
+                {
+                    today = today.AddDays(1);
+
+                    if (today.DayOfWeek == DayOfWeek.Sunday)
+                    {
+                        today = today.AddDays(1);
+
+                        day[i] = today.DayOfWeek.ToString();
+                    }
+                    else
+                    {
+                        day[i] = today.DayOfWeek.ToString();
+                    }
+                }                
+            }
+
+            foreach (var item in day)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
